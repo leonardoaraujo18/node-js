c400f3346786b6acffefa1d600e304fb54fe309a
Leonardo Araújo <progamadorweb18@gmail.com>
2025-08-25 15:01:43 -0300

    Criando rota com POST

--- projeto/src/app.js
+++ projeto/src/app.js
@@ -2,13 +2,16 @@ import express from 'express';
 
 const app = express();
 
+// indicar para o express ler body com json
+app.use(express.json());
+
 // mock
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
     {id: 2, selecao: 'Suiça', grupo: 'G'},
     {id: 3, selecao: 'Sérvia', grupo: 'G'},
     {id: 4, selecao: 'Camarões', grupo: 'G'},
-]
+];
 
 // criar rota ou raiz
 app.get('/', (req, res) =>{
@@ -17,6 +20,11 @@ app.get('/', (req, res) =>{
 
 app.get('/selecoes', (req, res) =>{
     res.status(200).send(selecoes)
+})
+
+app.post('/selecoes', (req, res) =>{
+    selecoes.push(req.body)
+    res.status(201).send('Seleção cadastrada com sucesso!')
 })
 
 export default app
