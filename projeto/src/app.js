5e73602605de07fa0aaf87580f6c91e609e99d9f
Leonardo Araújo <progamadorweb18@gmail.com>
2025-08-26 08:18:24 -0300

    Criando rota DELETE

--- projeto/src/app.js
+++ projeto/src/app.js
@@ -9,9 +9,19 @@ app.use(express.json());
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
     {id: 2, selecao: 'Suiça', grupo: 'G'},
-    {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'},
+    {id: 3, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Sérvia', grupo: 'G'},
 ];
+
+// Busca as seleções por ID
+function buscarSelecao(id){
+    return selecoes.filter(selecao => selecao.id == id)
+}
+
+// Busca as seleções pela posição
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
 
 // criar rota ou raiz
 app.get('/', (req, res) =>{
@@ -22,9 +32,19 @@ app.get('/selecoes', (req, res) =>{
     res.status(200).send(selecoes)
 })
 
+app.get('/selecoes/:id', (req, res) =>{
+    res.json(buscarSelecao(req.params.id));
+})
+
 app.post('/selecoes', (req, res) =>{
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com sucesso!')
+})
+
+app.delete('/selecoes/:id', (req,res) =>{
+    let index = buscarIndexSelecao(req.params.id);
+    selecoes.splice(index, 1);
+    res.send(`Seleção com id ${req.params.id} excluída com sucesso!`);
 })
 
 export default app
